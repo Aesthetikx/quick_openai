efa072667d2a2d0e9c35cf7f1d72bc8fd3ae7930
John DeSilva <desilvjo@umich.edu>
2022-12-14 11:19:52 -0500

    Add vcr cassettes

--- spec/quick_openai_spec.rb
+++ spec/quick_openai_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe QuickOpenai do
     expect(QuickOpenai::VERSION).not_to be nil
   end
 
-  it "can use gpt3" do
+  it "can use gpt3", :vcr do
     expect("What is two plus two, as a single digit?".gpt3.to_i).to eq(4)
   end
 
-  it "can use dalle2" do
+  it "can use dalle2", :vcr do
     expect("a drawing of a cat in a hat".dalle2).to be_a(Tempfile)
   end
 end
