d4f1f64ba132860d76a139fc0745a23f50f213af
John DeSilva <desilvjo@umich.edu>
2022-12-14 11:16:45 -0500

    Install vcr

--- Gemfile
+++ Gemfile
@@ -12,3 +12,5 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 
 gem "rubocop", "~> 1.21"
+
+gem "vcr", "~> 6"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require "quick_openai"
 
 require "dotenv/load"
 
+require "vcr"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -13,5 +15,17 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+
+  if ENV.key?('OPENAI_ACCESS_TOKEN')
+    config.filter_sensitive_data('<OPENAI_ACCESS_TOKEN>') do
+      ENV.fetch('OPENAI_ACCESS_TOKEN')
+    end
   end
 end
