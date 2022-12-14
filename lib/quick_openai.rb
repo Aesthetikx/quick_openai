6ae3d713de4da53c919a43a73681858082b86768
John DeSilva <desilvjo@umich.edu>
2022-12-14 10:49:35 -0500

    Genesis commit

---
+++ lib/quick_openai.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "quick_openai/version"
+
+module QuickOpenai
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/quick_openai_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe QuickOpenai do
+  it "has a version number" do
+    expect(QuickOpenai::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
