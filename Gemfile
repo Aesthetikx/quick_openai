33009e6f8279bea58158436e52bc04fa2ff98141
John DeSilva <desilvjo@umich.edu>
2022-12-14 11:03:50 -0500

    Install dotenv

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in quick_openai.gemspec
 gemspec
 
+gem "dotenv"
+
 gem "rake", "~> 13.0"
 
 gem "rspec", "~> 3.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 
 require "quick_openai"
 
+require "dotenv/load"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
